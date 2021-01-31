6223f3c325891e0bb929160a1090de74d64e1e24
Alberto Pluecker <albertopluecker@gmail.com>
2021-01-31 11:37:42 +0100

    Changed the grid items to be created as an array. createGrid() changed accordingly.

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 
-// Generating the 4x4 grid
 
-var gridContainer = document.querySelector('#grid-container');
-var itemNumber = 1; /* each item in the grid is assigned an itemNumber
-                        from 1 to 16*/
 
-for(j = 1 ; j < 5 ; j++){
-  for(i = 1 ; i < 5 ; i++){
-    console.log('( ' + i + ' , ' + j + ' )');
-    var item = document.createElement('item' + itemNumber);
-    console.log(item);
-    gridContainer.appendChild(item);
-    itemNumber = itemNumber +1;
+const container = document.querySelector('.container');
+let gridItem = [];
+
+
+// Generating the grid
+function returnGridsize(){
+  return 4;
+}
+
+
+function createGrid(){
+  let totalItems = returnGridsize * returnGridsize;
+  container.style.gridTemplateColumns = (`repeat(${returnGridsize}, 1fr`);
+  container.style.gridTemplateRows = (`repeat(${returnGridsize}, 1fr`);
+  for(i=0; i < totalItems ; i++){
+    gridItem[i] = document.createElement("div");
+    gridItem[i].classList.add("grid-item");
+    gridItem[i].addEventListener('mouseover', changeColor());
+    gridContainer.appendChild(gridItem[i]);
   }
+}
+
+function changeColor(e){
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
